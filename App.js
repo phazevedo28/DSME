0ea0c828aa3277ee81dfa45520b305e37fe1221c
Mariany Ferreira da Silva <fsilvamariany@gmail.com>
2020-12-13 15:50:22 -0300

    refactor: Home and Details screens

--- App.js
+++ App.js
@@ -1,43 +1,12 @@
-// In App.js in a new project
-
 import React from 'react';
-
-import { Button, View, Text } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import Home from './screens/Home';
+import Details from './screens/Details';
+
 import { get } from './api';
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
-  );
-}
-
-function DetailsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.push('Details')}
-      />
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-      <Button
-        title="Go back to first screen in stack"
-        onPress={() => navigation.popToTop()}
-      />
-    </View>
-  );
-}
 
 const Stack = createStackNavigator();
 
@@ -48,10 +17,10 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home">
-          {props => <HomeScreen {...props} />}
+          {props => <Home {...props} />}
         </Stack.Screen>
         <Stack.Screen name="Details">
-          {props => <DetailsScreen {...props} />}
+          {props => <Details {...props} />}
         </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
